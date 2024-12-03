e052ecf11b9b81709431f5370be1b4533472e6e7
natalia agapova <agapnatalya004@gmail.com>
2024-12-04 02:44:14 +0300

    reinstalled sumo

--- src/test_traci.py
+++ src/test_traci.py
@@ -1,14 +1,5 @@
+import os
 import traci
 
-def test_traci_connection():
-    # Start SUMO with the TraCI interface
-    traci.start(["sumo", "-c", "data/simulation_config.sumocfg"])
-
-    # Run the simulation for a few steps
-    for step in range(10):
-        traci.simulationStep()
-
-    traci.close()
-
-if __name__ == "__main__":
-    test_traci_connection()
+print("SUMO_HOME:", os.environ.get("SUMO_HOME"))
+print("traci imported successfully!")
